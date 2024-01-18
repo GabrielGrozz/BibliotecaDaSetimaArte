2b0869d119488899f29fa7c9c49ab83b6d7d093f
GabrielGrozz <gabrielgrozz.net@gmail.com>
2024-01-18 17:42:17 -0300

    add context class

--- BibliotecaDaSetimaArte/Context/AppDbContext.cs
+++ BibliotecaDaSetimaArte/Context/AppDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace BibliotecaDaSetimaArte.Context
+﻿using BibliotecaDaSetimaArte.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaDaSetimaArte.Context
 {
-    public class Class
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        DbSet<Movie> Movies { get; set; }
+        DbSet<Category> Categories { get; set; }
     }
 }
